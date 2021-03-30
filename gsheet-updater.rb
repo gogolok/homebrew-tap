fef7a81fd272a6d29c9ad67b1593ead966f54718
Robert Gogolok <gogolok@gmail.com>
2021-03-30 13:24:41 +0200

    gsheet-uploader 1.3.0

--- gsheet-updater.rb
+++ gsheet-updater.rb
@@ -3,11 +3,11 @@ require 'formula'
 class GsheetUpdater < Formula
   desc 'gsheet-updater'
   homepage 'https://github.com/gogolok/gsheet-updater'
-  version 'v1.2.0'
+  version 'v1.3.0'
 
   if OS.mac?
-    url 'https://github.com/gogolok/gsheet-updater/releases/download/v1.2.0/gsheet-updater-macos'
-    sha256 '9c61714dc20af37ee3529f836ec6c54bbf5b9d401e13aefda49d17d6872a3e64'
+    url 'https://github.com/gogolok/gsheet-updater/releases/download/v1.3.0/gsheet-updater-macos'
+    sha256 '300e47318d80d51e82d2b8aa7610a0647d90193c7b1d8fbdb6e1fb60de26b0f4'
   end
 
   depends_on :arch => :x86_64
